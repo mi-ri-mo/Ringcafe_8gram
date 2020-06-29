f3d7430ae3fd494a9f946a80cc8e335e45ddc5b5
JIiminIT <jiminit27@gmail.com>
2020-06-30 08:55:52 +0900

    nav scroll animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+// nav 스크롤 색 변경
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 1) {
+      $(".header").css("background" , "#FFFFFF"); // 흰색
+    }
+    else{
+      $(".header").css("background" , "#00ff0000");   // 투명색
+    }
+  })
+})
+
+
 // sild 효과
 let diff = 0;
 let ticking = false;
@@ -21,4 +35,6 @@ list.addEventListener('wheel', function(e) {
   }
   ticking = true;
 }, { passive: true });
+
+
 
