9385710aa10707b66c74e1ccc6c02e893689941e
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-11-15 19:48:21 +0900

    모바일 네비게이션 수정, 분기점 변경

--- js/nav.js
+++ js/nav.js
@@ -15,4 +15,14 @@ $(document).ready(function(){
     })
 })
 
+var toggle = false;
 
+const onToggle = () => {
+    toggle = !toggle;
+    if(toggle) {
+        document.getElementById("nav").style.background="white";
+    }
+    else {
+        document.getElementById("nav").style.background = "rgba(0,0,0,0)";
+    }
+}
