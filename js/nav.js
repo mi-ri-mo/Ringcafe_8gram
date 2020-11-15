6dedb3bcf02471cc2b7266e28d47676e7a3f8847
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2020-11-15 18:14:45 +0900

    main 반응형 1차

--- js/nav.js
+++ js/nav.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
         if (scroll > 1) {
             $("#web-nav").css("background" , "#FFFFFF");
             $(".navli").attr("style", "color: #000000 !important");
+            $("#web-nav").css("border-bottom" , "0.1px solid #c9c9c9");
         }
         else {
             $("#web-nav").css("background" , "#00ff0000");
             $(".navli").attr("style", "color: #FFFFFF !important");
+            $("#web-nav").css("border-bottom" , "0");
         }
     })
 })
